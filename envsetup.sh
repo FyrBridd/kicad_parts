47b764b9778d6787b88f524479bba3f487e3c1fc
Philip Bassett <philipbassett101@googlemail.com>
2025-03-24 17:01:37 +0000

    More resistors added, capacitors started

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,5 @@
 # define all of your libs here -- should be a CSV file for each lib
-GPLMLIBS="ana cap con cpd dio ics ind mpu mcu pwr rfm res reg xtr osc opt art swi"
+GPLMLIBS="capacitors resistors"
 
 DBFILE=./database/parts.sqlite
 
